6bd6379e3f9f676b43b930bfbdca99bb46111659
weixuancai <39092171+weixuancai@users.noreply.github.com>
2018-11-21 16:16:43 +0800

    v2.2

--- ping.py
+++ ping.py
@@ -1,27 +1,39 @@
-import os
-import json
 import subprocess
+import sys
+import json
 
 with open('communication.json') as json_data_file:
     conf = json.load(json_data_file)
+# list fail
+fail = list()
+fail_ip = list()
 
-def check_ping():
-    hostname = "google.com"
-    ip = "140.118.121.2"
-    for i in conf:
-        ip = i[u'IP']
-        print ip
-        response = os.system("ping -c 1 " + ip)
-    # and then check the response...
-        if response == 0:
-            pingstatus = "Network Active"
-        else:
-            pingstatus = "Network Error"
-        print pingstatus
-    # return pingstatus
-
-# check_ping()
-hostname = "google.com"
 def ping():
-    out = subprocess.check_output("ping -c 1 " + hostname)
+    for i in conf:
+        host = i[u'IP']
+        # cmd = ['ping', '-c2', '-W 5', host ]
+        cmd = ['ping', '-c1', host ]
+        # print cmd
+        done = False
+        timeout = 1 # default time out after ten times, set to -1 to disable timeout
+
+        print i[u'Device_ID']," : Ping ...",
+        while not done and timeout:
+                response = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+                stdout, stderr = response.communicate()
+                if response.returncode == 0:
+                    print "OK"
+                    done = True
+                else:
+                    # sys.stdout.write('.')
+                    timeout -= 1
+                    fail.append(str(i[u'Device_ID']))
+                    fail_ip.append(str(i[u'IP']))
+                    
+        if not done:
+            print "Failed to respond"
+
 ping()
+print "==========================================================="
+for index,item in enumerate(fail):
+    print item," | ",fail_ip[index]
